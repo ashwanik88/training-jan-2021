9d02f96ea7574c69fb06e3a404b9e7da2e2d671b
Ashwani <ashwani.k88@gmail.com>
2021-06-15 12:50:44 +0530

    Remember checkbox added using cookies

--- admin/library/index_lib.php
+++ admin/library/index_lib.php
@@ -1,23 +1,42 @@
 <?php
 
+if($_COOKIE){
+	if(isset($_COOKIE['username']) && !empty($_COOKIE['username']) && isset($_COOKIE['password']) && !empty($_COOKIE['password'])){
+		$username = $_COOKIE['username'];
+		$password = $_COOKIE['password'];
+		authUser($con, $username, $password);
+	}
+	
+}
+
 $error = '';
 if($_POST){
-	$username = $_POST['username'];
-	$password = $_POST['password'];
-	
-	$sql = "SELECT * FROM users WHERE username='". $username ."' AND password='". md5($password) ."'";
-	
-	$rs = mysqli_query($con, $sql);
-	
-	if(mysqli_num_rows($rs)){
-		
-		$rec = mysqli_fetch_assoc($rs);
-		
-		$_SESSION['login'] = $rec;
-		
-		addAlert('success', 'Successfully logged in!');
-		redirect('dashboard.php');
-	}else{
-		addAlert('danger', 'Incorrect username/password!');
+	if(isset($_POST['username']) && !empty($_POST['username']) && isset($_POST['password']) && !empty($_POST['password'])){
+		$username = $_POST['username'];
+		$password = md5($_POST['password']);
+		authUser($con, $username, $password);
 	}
+}
+
+function authUser($con, $username, $password){
+$sql = "SELECT * FROM users WHERE username='". $username ."' AND password='". $password ."'";
+
+$rs = mysqli_query($con, $sql);
+
+if(mysqli_num_rows($rs)){
+	
+	$rec = mysqli_fetch_assoc($rs);
+	
+	if(isset($_POST['remember']) && !empty($_POST['remember'])){
+		setcookie('username', $username, time() + 60 * 60 * 24 * 30);
+		setcookie('password', $password, time() + 60 * 60 * 24 * 30);
+	}
+	
+	$_SESSION['login'] = $rec;
+	
+	addAlert('success', 'Successfully logged in!');
+	redirect('dashboard.php');
+}else{
+	addAlert('danger', 'Incorrect username/password!');
+}	
 }
--- admin/logout.php
+++ admin/logout.php
@@ -1,5 +1,8 @@
-<?php require_once('includes/startup.php'); 
-
+<?php require_once('includes/startup.php');
+ 
+setcookie('username', $username, time() - 60 * 60 * 24 * 30);
+setcookie('password', $password, time() - 60 * 60 * 24 * 30);
+		
 unset($_SESSION['login']);
 
 addAlert('success', 'Successfully logged out!');
